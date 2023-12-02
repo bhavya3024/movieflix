b1c62ca07319520b544f632de4e7653f1291da87
Bhavya Dhiman <bhavyadhiman7@gmail.com>
2023-12-03 00:36:14 +0530

    fluent init

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FluentProvider, teamsLightTheme } from '@fluentui/react-components';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import App from './App';
+
+ReactDOM.render(
+  <FluentProvider theme={teamsLightTheme}>
     <App />
-  </React.StrictMode>,
-)
+  </FluentProvider>,
+  document.getElementById('root'),
+);
